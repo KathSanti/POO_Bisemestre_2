b762d4a8e21ed394fede008a1c492ca7ac515203
KatherineSantibanez <kath.santibanez@gmail.com>
2025-11-08 20:45:03 -0300

    Class prototipo Portal Salmott Puerto Montt

---
+++ FormativaSemana3Sumativa1/model/Proveedor.java
@@ -0,0 +1,23 @@
+package FormativaSemana3Sumativa1.model;
+import FormativaSemana3Sumativa1.model.DatosFicha;
+
+public class Proveedor extends DatosFicha {
+
+    private String CentroDeCosto;
+
+    public Proveedor(String nombre, String rut, String direccion, String telefono, String email, String CentroDeCosto){
+        super(nombre, rut, direccion, telefono, email);
+        this.CentroDeCosto = CentroDeCosto;
+    }
+
+    @Override
+    public void procesarInformacionFicha(){
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Centro de costo: " + CentroDeCosto);
+        System.out.println("Direccion: " + direccion);
+        System.out.println("Telefono: " + telefono);
+        System.out.println("Email: " + email);
+    }
+
+
+}
